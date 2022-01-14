2600f7d6cb9830263ad77ddf401ba2ac3ccb8428
David Boeke <david@boeke.com>
2022-01-14 16:49:37 -0500

    interval stuff

--- interval-prod/main.tf
+++ interval-prod/main.tf
@@ -18,23 +18,23 @@ variable "turbot_profile" {
 resource "turbot_policy_setting" "turbot_volume_interval" {
   resource           = "tmod:@turbot/aws-ec2#/control/types/volumeCmdb"
   type               = "tmod:@turbot/turbot#/policy/types/interval"
-  value              = "days: 2"
+  value              = "days: 3"
 }
 
 resource "turbot_policy_setting" "turbot_instance_interval" {
   resource           = "tmod:@turbot/aws-ec2#/control/types/instanceCmdb"
   type               = "tmod:@turbot/turbot#/policy/types/interval"
-  value              = "days: 2"
+  value              = "days: 3"
 }
 
 resource "turbot_policy_setting" "turbot_instance_tags_interval" {
   resource           = "tmod:@turbot/aws-ec2#/policy/types/instanceTagsTemplate"
   type               = "tmod:@turbot/turbot#/policy/types/interval"
-  value              = "days: 2"
+  value              = "days: 3"
 }
 
 resource "turbot_policy_setting" "turbot_volume_tags_interval" {
   resource           = "tmod:@turbot/aws-ec2#/policy/types/volumeTagsTemplate"
   type               = "tmod:@turbot/turbot#/policy/types/interval"
-  value              = "days: 2"
+  value              = "days: 3"
 }
