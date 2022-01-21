eb0879b007aafce31098b584e827deb760c8a70a
David Boeke <david@boeke.com>
2022-01-21 14:25:53 -0500

    new volume

--- tags-sandbox/snapshot_tags.tf
+++ tags-sandbox/snapshot_tags.tf
@@ -66,7 +66,7 @@ resource "turbot_policy_setting" "snapshot_tag_template" {
     {%- set assoc_vol_env = false -%}
     {#- grab Environment from Volume -#}
     {%- for env_tag_key in env_key_list -%}
-      {%- if $.vols.items %-}
+      {%- if $.vols.items -%}
         {%- if env_tag_key in $.vols.items[0].turbot.tags -%}
           {%- if $.vols.items[0].turbot.tags[env_tag_key] in tag_value_map -%}
             {%- set assoc_vol_env = $.vols.items[0].turbot.tags[env_tag_key] -%}
