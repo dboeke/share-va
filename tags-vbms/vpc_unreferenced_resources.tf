57bf499cbb9d5e22e30fab8cae8f38ec38811705
David Boeke <david@boeke.com>
2022-01-20 20:05:49 -0500

    roll out new tagging logic

---
+++ tags-vbms/vpc_unreferenced_resources.tf
@@ -0,0 +1,76 @@
+#####################################################################
+## Tagging for resources related to the VPC that have VPC References
+#####################################################################
+resource "turbot_policy_setting" "vpc_unreferenced_resource_tag_enforcement" {
+  for_each        = var.vpc_unreferenced_tags 
+  resource        = turbot_smart_folder.vaec_aws_tagging.id
+  type            = var.policy_map[each.key]
+  value           = each.value
+}
+
+resource "turbot_policy_setting" "vpc_unreferenced_resource_tag_template" {
+  for_each        = var.vpc_unreferenced_tags
+  resource        = turbot_smart_folder.vaec_aws_tagging.id
+  type            = var.policy_map_template[each.key]
+  # GraphQL to pull policy Statements
+  template_input  = <<-QUERY
+  {
+    region {
+        children(filter:"title:'/vaec/tag/*' resourceTypeId:tmod:@turbot/aws-ssm#/resource/types/ssmParameter resourceTypeLevel:self") {
+          items {
+            name: get(path: "Name")
+            value: get(path: "Value")
+          }
+        }
+      }
+    resource {
+      turbot {
+        tags
+      }
+    }
+  }
+  QUERY
+  
+  # Nunjucks template to set tags and check for tag validity.
+  template = <<-TEMPLATE
+    {#- --------------------------- -#}
+    {#-    initialize variables     -#}
+    {#- --------------------------- -#}
+    {%- set new_tags = "" -%}
+    {%- set required_tags = ${jsonencode(var.required_tags)} -%}
+    {%- set tag_value_map = ${jsonencode(var.wrong_tag_values)} -%}
+    {#- --------------------------- -#}
+    {#- set default tags from ssm   -#}
+    {#- --------------------------- -#}
+    {%- for ssm_param in $.region.children.items -%}
+      {%- if ssm_param.name in required_tags -%}
+        {%- set new_tags = new_tags + '- "' + required_tags[ssm_param.name] + '": ' -%}
+        {%- set new_tags = new_tags + '"' + ssm_param.value + '"\n' -%}
+      {%- endif -%}
+    {%- endfor -%}
+    {#- --------------------------- -#}
+    {#-     set environment tag     -#}
+    {#- --------------------------- -#}
+    {%- set env_tag = "null" -%}
+    {%- if "Environment" in $.resource.turbot.tags -%}
+      {%- if $.resource.turbot.tags["Environment"] in tag_value_map -%}
+        {%- set env_tag = '"' + tag_value_map[$.resource.turbot.tags["Environment"]] + '"\n'  -%}
+      {%- endif -%}
+    {%- elif "environment" in $.resource.turbot.tags -%}
+      {%- if $.resource.turbot.tags["environment"] in tag_value_map -%}
+        {%- set env_tag = '"' + tag_value_map[$.resource.turbot.tags["environment"]] + '"\n'  -%}
+      {%- endif -%}
+    {%- endif -%}
+    {%- if (env_tag == "null") and ("vaec:Environment" in $.resource.turbot.tags) -%}
+      {%- if $.resource.turbot.tags["vaec:Environment"] in tag_value_map -%}
+        {%- set env_tag = '"' + tag_value_map[$.resource.turbot.tags["vaec:Environment"]] + '"\n'  -%}
+      {%- endif -%}
+    {%- endif -%}
+    {%- set new_tags = new_tags + '- "vaec:Environment": ' + env_tag -%}
+    {#- --------------------------- -#}
+    {#-     output required tags    -#}
+    {#- --------------------------- -#}
+    {{ new_tags }}
+    TEMPLATE
+}
+
