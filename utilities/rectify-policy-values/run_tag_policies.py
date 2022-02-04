84939d7c7a1b000e94615a2b1d4f4e6866671c19
David Boeke <david@boeke.com>
2022-02-04 19:39:45 +0000

    separate

--- utilities/rectify-policy-values/run_tag_policies.py
+++ utilities/rectify-policy-values/run_tag_policies.py
@@ -1,23 +1,21 @@
 import turbot
+import vaec
 import click
 from sgqlc.endpoint.http import HTTPEndpoint
+import pprint
 import time
-import psycopg2
-import boto3
-
+import subprocess
+import os
 
 @click.command()
 @click.option('-p', '--profile', default="default", help="[String] Profile to be used from config file.")
 @click.option('-c', '--cooldown', default="60", help="[Integer] Seconds to wait between batches.")
-@click.option('--host', help="[String] fully qualified hostname of Turbot RDS instance.")
-@click.option('--port', default="5432", help="[Integer] ip port of rds instance.")
-@click.option('--region', default="us-gov-west-1", help="[String] region name of instance")
-@click.option('--timeout', default="20", help="[Float] how long to wait on graphql call to run policy values")
+@click.option('-t', '--timeout', default="10", help="[Integer] Seconds to wait for graphql response")
 @click.option('--state-filter', default="", help="[Float] e.g. state:error")
 
-
-def rectify(profile, cooldown, host, port, region, timeout, state_filter):
+def rectify(profile, cooldown, timeout, state_filter):
     config_file = None
+
     config = turbot.Config(config_file, profile)
     headers = {'Authorization': 'Basic {}'.format(config.auth_token)}
     endpoint = HTTPEndpoint(config.graphql_endpoint, base_headers=headers, timeout=float(timeout))
@@ -41,10 +39,19 @@ def rectify(profile, cooldown, host, port, region, timeout, state_filter):
         }
     '''
 
+    mutation = '''
+        mutation RunPolicy($input: RunPolicyInput!) {
+            runPolicy(input: $input) {
+                turbot {
+                    id
+                }
+            }
+        }
+    '''
+
     filter = "controlCategoryId:'tmod:@turbot/turbot#/control/categories/resourceTags'"
     if len(state_filter):
         filter = f"{state_filter} {filter}"
-
     print(f"Using filter: {filter}")
     paging = None
     print("Starting...")
@@ -53,32 +60,20 @@ def rectify(profile, cooldown, host, port, region, timeout, state_filter):
         variables = {'filter': filter, 'paging': paging}
         result = endpoint(query, variables)
 
-        session = boto3.Session()
-        client = session.client('rds')
-        token = client.generate_db_auth_token(DBHostname=host, Port=port, DBUsername="turbot", Region=region)
-
-        try:
-            conn = psycopg2.connect(host=host, port=port, database="turbot", user="turbot", password=token, sslmode='require', sslrootcert="SSLCERTIFICATE")
-        except Exception as e:
-            print("Database connection failed due to {}".format(e))    
-                
         for item in result['data']['targets']['items']:
-            with conn:
-                with conn.cursor() as curs:
-                    if 'id' in item['resource'] and item['resource']['id'] and len(item['resource']['id']) > 12:
-                        print("Rectifying Resource: {}".format(item['resource']['id']))
-                        rec_query = "select * from rectify_policy_values({}::bigint);".format(item['resource']['id'])
-                        curs.execute(rec_query)
-                        query_results = curs.fetchall()
-                        print(query_results)
+            
+            print("running policy value for: {}".format(item['turbot']['id']))
+            vars = {'input': {'id': item['turbot']['id']}}
+            try:
+                run = endpoint(mutation, vars)
+                print(run)
+            except Exception as e:
+                print(e)
 
         if not result['data']['targets']['paging']['next']:
             break
         else:
             paging = result['data']['targets']['paging']['next']
-
-        conn.close()
-
         print("Pausing for {} seconds".format(cooldown))
         time.sleep(int(cooldown))
 
