128b41ff2bd312ed7083e8b739a4e52b259ae841
David Boeke <david@boeke.com>
2022-02-11 13:20:52 -0500

    mod upload

--- utilities/mod-updates/update_mods.py
+++ utilities/mod-updates/update_mods.py
@@ -3,19 +3,21 @@ import click
 from sgqlc.endpoint.http import HTTPEndpoint
 import subprocess
 import os
+import time
 
 @click.command()
 @click.option('-p', '--profile', default="default", help="[String] Profile to be used from config file.")
 @click.option('-o', '--output-dir', default=".", help="Path to download folder", type=click.Path(exists=True, file_okay=False))
+@click.option('-c', '--cooldown', default=300, help="[Integer] Seconds to wait between mod updates. Default == 5m", type=int)
 @click.option('--download/--skip-download', default=True )
-@click.option('--timeout', default=120, help="[Integer] Seconds to wait for graphql response. Default == 120s", type=float)
 
-def update_mods(profile, output_dir, timeout, download):
+
+def update_mods(profile, output_dir, cooldown, download):
     config_file = None
 
     config = turbot.Config(config_file, profile)
     headers = {'Authorization': 'Basic {}'.format(config.auth_token)}
-    endpoint = HTTPEndpoint(config.graphql_endpoint, base_headers=headers, timeout=timeout)
+    endpoint = HTTPEndpoint(config.graphql_endpoint, base_headers=headers, timeout=240)
 
     query = '''
         query Mods($status: [ModVersionStatus!]) {
@@ -48,6 +50,9 @@ def update_mods(profile, output_dir, timeout, download):
             subprocess.run(download_cmd, shell=True)
         upload_cmd = f"turbot up --zip-file {output_dir}{sep}turbot_{mod['name']}.zip"
         subprocess.run(upload_cmd, shell=True)
+
+        print("Pausing for {} seconds".format(cooldown))
+        time.sleep(cooldown)
 
 if __name__ == "__main__":
     update_mods()
