93b699c342d52eb67ac624a2c47f8daf0ece9b53
David Boeke <david@boeke.com>
2022-03-09 13:14:45 -0500

    azure disk

--- reports/encryption_report/azure_encryption_report.py
+++ reports/encryption_report/azure_encryption_report.py
@@ -32,6 +32,13 @@ def run_report(profile, bucket):
                     trunk {
                         title
                     }
+                    parent {
+                        type {
+                            category {
+                                title
+                            }
+                        }
+                    }
                 }
                 paging {
                     next
@@ -41,7 +48,7 @@ def run_report(profile, bucket):
     '''
 
     az_disk_filter = "resourceId:'210275942479682' resourceTypeId:'tmod:@turbot/azure-compute#/resource/types/disk' resourceTypeLevel:self limit:250"
-    az_disk_filter = "resourceId:210275942479682 level:self,descendant resourceTypeId:tmod:@turbot/azure-compute#/resource/types/disk resourceTypeLevel:self,descendant limit:250"
+    az_disk_filter = "resourceId:210275942479682 level:self,descendant resourceTypeId:tmod:@turbot/azure-compute#/resource/types/disk resourceTypeLevel:self,descendant sort:title limit:250"
     items = []
     az_disks = []
     max_len = 0
