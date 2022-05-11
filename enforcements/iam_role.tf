0583a12dc32fa835fa94c175cc79b1c182cb211d
David Boeke <david@boeke.com>
2022-05-11 16:02:35 -0400

    policy exceptions

--- enforcements/iam_role.tf
+++ enforcements/iam_role.tf
@@ -51,6 +51,17 @@ resource "turbot_policy_setting" "aws_iam_role_policy_attachments_approved" {
     EOT
 }
 
+# AWS > IAM > Role > Policy Attachments > Approved > Rules
+resource "turbot_policy_setting" "aws_iam_role_policy_attachments_approved_rules" {
+  resource = turbot_smart_folder.iam_controls_enforce.id
+  type     = "tmod:@turbot/aws-iam#/policy/types/rolePolicyAttachmentsApprovedRules"
+  value    = <<-EOT
+    REJECT $.PolicyName:/^.+FullAccess.*$/
+    REJECT $.PolicyName:AdministratorAccess
+    APPROVE *
+    EOT
+}
+
 # # Unable to set these policies due to bug. Mods team is looking into it
 # # AWS > IAM > Role > Inline Policy > Statements > Approved > Administrator Access
 # resource "turbot_policy_setting" "aws_iam_role_inline_policy_statements_approved_admin_access" {
